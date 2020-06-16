96acca15c251c66d7e90ec53667f6c2677588084
Germain SIPIERE <43010217+germainsip@users.noreply.github.com>
2020-06-16 15:57:45 +0200

    première méthode dans l'interface+annotation

--- app/src/main/java/com/germain/retrofitandroid/ApiService.java
+++ app/src/main/java/com/germain/retrofitandroid/ApiService.java
@@ -3,8 +3,9 @@ package com.germain.retrofitandroid;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.GET;
 
 public interface ApiService {
-
+    @GET("posts")
     Call<List<Post>> getPosts();
 }
