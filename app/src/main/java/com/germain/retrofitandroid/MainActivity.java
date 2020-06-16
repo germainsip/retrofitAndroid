0554612554dd9cd3cc341870f171aa13b6d26884
Germain SIPIERE <43010217+germainsip@users.noreply.github.com>
2020-06-16 17:17:33 +0200

    retrocompatibilité jdk8 et première requ complète

--- app/src/main/java/com/germain/retrofitandroid/MainActivity.java
+++ app/src/main/java/com/germain/retrofitandroid/MainActivity.java
@@ -1,14 +1,62 @@
 package com.germain.retrofitandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+textResult = findViewById(R.id.text_result);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+
+        Call<List<Post>> call = apiService.getPosts();
+
+        call.enqueue(new Callback<List<Post>>() {
+            private static final String TAG = "TEST_AFFICHAGE";
+
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if (!response.isSuccessful()) {
+                    textResult.setText("Oups! le code de réponse est " + response.code());
+                    return;
+                }
+
+                List<Post> posts = response.body();
+
+                for (Post post : posts) {
+                    String content = "";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "User ID: " + post.getUserId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Text: " + post.getText() + "\n\n";
+                    Log.d(TAG, "onResponse: "+content);
+                    textResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textResult.setText(t.getMessage());
+            }
+        });
     }
 }
